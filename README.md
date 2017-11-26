816e40536ec2334fd9160b947cbcbfa504cf5ae9
Alexander Shepetko <a@shepetko.com>
2017-11-27 00:37:42 +0200

    Release 1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# PytSite File Plugin
+
+
+## Changelog
+
+
+### 1.0 (2017-11-27)
+
+First release.
---
+++ _http_api_controllers.py
@@ -0,0 +1,59 @@
+"""PytSite File HTTP API Endpoints
+"""
+from typing import List as _List
+from os import unlink as _unlink
+from pytsite import util as _util, routing as _routing
+from plugins import file as _file, auth as _auth
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class Post(_routing.Controller):
+    """Upload a file
+    """
+
+    def exec(self) -> _List[str]:
+        if _auth.get_current_user().is_anonymous:
+            raise self.forbidden()
+
+        if not self.files:
+            raise RuntimeError('No files received')
+
+        r = []
+        for field_name, f in self.files.items():
+            tmp_file_path = _util.mk_tmp_file()[1]
+            f.save(tmp_file_path)
+
+            file = _file.create(tmp_file_path, f.filename, 'Uploaded via HTTP API')
+            _unlink(tmp_file_path)
+
+            r.append({
+                'uid': str(file.uid),
+            })
+
+        # Request was from CKEditor
+        if self.arg('CKEditor') and self.arg('CKEditorFuncNum'):
+            script = 'window.parent.CKEDITOR.tools.callFunction("{}", "{}", "");' \
+                .format(self.arg('CKEditorFuncNum'), _file.get(r[0]['uid']).get_url())
+
+            # CKEditor requires such response format
+            r = '<script type="text/javascript">{}</script>'.format(script)
+
+        return r
+
+
+class Get(_routing.Controller):
+    """Get information about a file
+    """
+
+    def exec(self) -> dict:
+        if _auth.get_current_user().is_anonymous:
+            raise self.forbidden()
+
+        try:
+            return _file.get(self.arg('uid')).as_jsonable(**dict(self.args))
+
+        except _file.error.FileNotFound as e:
+            raise self.not_found(str(e))

8ee2d5362ed4c37330baaf3bc805d545c6b666ed
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:11:21 +0200

    Release 1.2

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 ## Changelog
 
 
+### 1.2 (2017-12-13)
+
+Support for PytSite-7.0.
+
+
 ### 1.1 (2017-12-02)
 
 Support for PytSite-6.1.
-
 
 
 ### 1.0 (2017-11-27)
--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
 """PytSite File UI Plugin
 """
-
-# Public API
-from . import _widget as widget
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def _init():
+if _plugman.is_installed(__name__):
+    # Public API
+    from . import _widget as widget
+
+
+def plugin_load():
     from pytsite import tpl, lang
     from plugins import assetman, http_api
     from . import _http_api_controllers
@@ -18,12 +20,9 @@ def _init():
     tpl.register_package(__name__)
 
     assetman.register_package(__name__)
-    assetman.t_less(__name__ + '@**')
-    assetman.t_js(__name__ + '@**')
+    assetman.t_less(__name__)
+    assetman.t_js(__name__)
     assetman.js_module('file_ui-widget-files-upload', __name__ + '@js/widget-files-upload')
 
     http_api.handle('POST', 'file', _http_api_controllers.Post, 'file_ui@post')
     http_api.handle('GET', 'file/<uid>', _http_api_controllers.Get, 'file_ui@get')
-
-
-_init()

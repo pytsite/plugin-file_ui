adbcb50249f9e070e604ba200daa34f0a18ee1db
Alexander Shepetko <a@shepetko.com>
2017-12-21 12:27:29 +0200

    Minor update 1.2.2

--- __init__.py
+++ __init__.py
@@ -11,16 +11,29 @@ if _plugman.is_installed(__name__):
     from . import _widget as widget
 
 
-def plugin_load():
-    from pytsite import lang
+def _register_assetman_resources():
     from plugins import assetman
 
-    lang.register_package(__name__)
+    if not assetman.is_package_registered(__name__):
+        assetman.register_package(__name__)
+        assetman.t_less(__name__)
+        assetman.t_js(__name__)
+        assetman.js_module('file_ui-widget-files-upload', __name__ + '@js/widget-files-upload')
 
-    assetman.register_package(__name__)
-    assetman.t_less(__name__)
-    assetman.t_js(__name__)
-    assetman.js_module('file_ui-widget-files-upload', __name__ + '@js/widget-files-upload')
+    return assetman
+
+
+def plugin_install():
+    assetman = _register_assetman_resources()
+    assetman.build(__name__)
+    assetman.build_translations()
+
+
+def plugin_load():
+    from pytsite import lang
+
+    lang.register_package(__name__)
+    _register_assetman_resources()
 
 
 def plugin_load_uwsgi():
@@ -32,11 +45,3 @@ def plugin_load_uwsgi():
 
     http_api.handle('POST', 'file', _http_api_controllers.Post, 'file_ui@post')
     http_api.handle('GET', 'file/<uid>', _http_api_controllers.Get, 'file_ui@get')
-
-
-def plugin_install():
-    from plugins import assetman
-
-    plugin_load()
-    assetman.build(__name__)
-    assetman.build_translations()
